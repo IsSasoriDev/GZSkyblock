4202812bb796cb4ea84ff750a084bdca66eb9254
SasoriDev <57101766+IsSasoriDev@users.noreply.github.com>
2025-03-01 13:29:55 +0200

    Update DataManager.php

--- src/SkyblockCore/DataManager.php
+++ src/SkyblockCore/DataManager.php
@@ -12,60 +12,36 @@ class DataManager {
 
     public function __construct(Main $plugin) {
         $this->plugin = $plugin;
-        $this->playerData = new Config($plugin->getDataFolder() . "players.yml", Config::YAML);
+        $this->playerData = new Config($this->plugin->getDataFolder() . "players.yml", Config::YAML);
     }
 
     public function initializePlayer(Player $player): void {
-        if(!$this->playerData->exists($player->getName())) {
-            $this->playerData->set($player->getName(), [
+        $name = $player->getName();
+        if(!$this->playerData->exists($name)) {
+            $this->playerData->set($name, [
                 'island' => null,
                 'xp' => 0,
                 'level' => 1,
-                // Remove balance field
                 'completed_quests' => [],
                 'active_quests' => []
             ]);
             $this->playerData->save();
         }
     }
-}
+
     public function getIslandData(Player $player): array {
         return $this->playerData->get($player->getName())['island'] ?? [];
     }
 
     public function saveIslandData(Player $player, array $data): void {
-        $name = $player->getName();
-        $current = $this->playerData->get($name, []);
-        $current['island'] = array_merge($current['island'] ?? [], $data);
-        $this->playerData->set($name, $current);
-        $this->playerData->save();
-    }
-
-    public function completeQuest(Player $player, string $questId): void {
-        $name = $player->getName();
-        $data = $this->playerData->get($name);
-        $data['completed_quests'][$questId] = time();
-        unset($data['active_quests'][$questId]);
-        $this->playerData->set($name, $data);
-        $this->playerData->save();
-    }
-
-    public function updateQuestProgress(Player $player, string $questId, int $progress): void {
-        $name = $player->getName();
-        $data = $this->playerData->get($name);
-        $data['active_quests'][$questId] = $progress;
-        $this->playerData->set($name, $data);
+        $current = $this->playerData->get($player->getName());
+        $current['island'] = $data;
+        $this->playerData->set($player->getName(), $current);
         $this->playerData->save();
     }
 
     public function getPlayerStats(Player $player): array {
-        return $this->playerData->get($player->getName(), [
-            'xp' => 0,
-            'level' => 1,
-            'balance' => 0.0,
-            'completed_quests' => [],
-            'active_quests' => []
-        ]);
+        return $this->playerData->get($player->getName());
     }
 
     public function saveAll(): void {
